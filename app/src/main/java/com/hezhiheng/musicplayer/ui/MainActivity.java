f814b170dad2eb9de6d960a892d8786e1f9e6824
unknown <hzh_innovation@163.com>
2020-12-15 23:56:04 +0800

    [hzh] feat: add top user info

--- app/src/main/java/com/hezhiheng/musicplayer/ui/MainActivity.java
+++ app/src/main/java/com/hezhiheng/musicplayer/ui/MainActivity.java
@@ -1,7 +1,11 @@
 package com.hezhiheng.musicplayer.ui;
 
 import android.app.Activity;
+import android.graphics.Rect;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
 
 import androidx.annotation.Nullable;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -19,6 +23,12 @@ import butterknife.ButterKnife;
 public class MainActivity extends Activity {
     @BindView(R.id.created_music_list_container)
     RecyclerView mMusicListContainer;
+    @BindView(R.id.main_page_header_container)
+    LinearLayout mMainPageHeaderContainer;
+    @BindView(R.id.main_page_scroll)
+    ScrollView mScrollview;
+    @BindView(R.id.main_page_user_info_container)
+    LinearLayout mMainUserInfoContainer;
 
     private List<MusicList> lists = MusicList.createList();
 
@@ -31,5 +41,20 @@ public class MainActivity extends Activity {
         MusicListAdapter musicListAdapter = new MusicListAdapter(lists, this);
         mMusicListContainer.setAdapter(musicListAdapter);
         mMusicListContainer.setLayoutManager(new LinearLayoutManager(this));
+
+        mScrollview.setOnScrollChangeListener(new View.OnScrollChangeListener() {
+            @Override
+            public void onScrollChange(View v, int scrollX, int scrollY, int oldScrollX, int oldScrollY) {
+                Rect scrollRect = new Rect();
+                mScrollview.getHitRect(scrollRect);
+                if (mMainUserInfoContainer.getLocalVisibleRect(scrollRect)) {
+                    mMainUserInfoContainer.setVisibility(View.VISIBLE);
+                    mMainPageHeaderContainer.setVisibility(View.INVISIBLE);
+                } else {
+                    mMainUserInfoContainer.setVisibility(View.INVISIBLE);
+                    mMainPageHeaderContainer.setVisibility(View.VISIBLE);
+                }
+            }
+        });
     }
 }
