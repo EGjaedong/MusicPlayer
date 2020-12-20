0a26b591088a4d0ced4b863d131b1b3dc242865e
unknown <hzh_innovation@163.com>
2020-12-20 13:08:06 +0800

    [hzh] feat: format main_acitivty adn tab_bar_acitivty

--- app/src/main/java/com/hezhiheng/musicplayer/ui/main/TabBarContainer.java
+++ app/src/main/java/com/hezhiheng/musicplayer/ui/main/TabBarContainer.java
@@ -11,10 +11,24 @@ import androidx.fragment.app.Fragment;
 
 import com.hezhiheng.musicplayer.R;
 
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
+
 public class TabBarContainer extends Fragment {
+
+    private Unbinder mBind;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.tab_bar_layout, container, false);
+        View view = inflater.inflate(R.layout.tab_bar_layout, container, false);
+        mBind = ButterKnife.bind(this, view);
+        return view;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mBind.unbind();
     }
 }
